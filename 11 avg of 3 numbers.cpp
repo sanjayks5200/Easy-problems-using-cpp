4d9d531bd9e06dd68f1f5d62dff9e89c55d0d5e9
sanjayks5200 <kssanjay5200@gmail.com>
2025-09-03 22:06:24 +0530

    Create 11 avg of 3 numbers.cpp

---
+++ 11 avg of 3 numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  float num1,num2,num3,avg;
+  cout<<"Enter the 3 numbers : "<<endl;
+  cin>>num1>>num2>>num3;
+  
+  cout<<" The average of the 3 numbers is  "<<avg<<endl;
+return 0  
+}
