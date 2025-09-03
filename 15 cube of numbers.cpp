135762ab465894bfeb3a3ef5044d4c5dd69fae05
sanjayks5200 <kssanjay5200@gmail.com>
2025-09-03 22:27:57 +0530

    Create 15 cube of numbers.cpp

---
+++ 15 cube of numbers.cpp
@@ -0,0 +1,12 @@
+include <iostream>
+using namespace std;
+
+int main(){
+    int num, cube;
+    cout << "Enter the Number to find the Cube: ";
+    cin >> num1;
+    sqaure=(num*num*num);
+    cout << "The Cube of Number is : "<<cube<<endl;
+  
+    return 0;
+}
