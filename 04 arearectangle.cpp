0d42c977df66677ef236c8002ba27145f7003d07
sanjayks5200 <kssanjay5200@gmail.com>
2025-09-03 21:07:07 +0530

    Create 04 arearectangle.cpp

---
+++ 04 arearectangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float length,width,area;
+
+    cout <<"length :";
+    cin >> length;
+    cout << "width :";
+    cin >> width;
+
+    area=length*width;
+    cout << "Area :" << area;
+
+    return 0;
+}
