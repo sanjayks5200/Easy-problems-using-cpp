ffe9098204b881a37de50adfda82215ffce982ed
sanjayks5200 <kssanjay5200@gmail.com>
2025-09-03 21:11:29 +0530

    Create 05 celsius.cpp

---
+++ 05 celsius.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float celsius, farenheit;
+
+    cout<<"temperature in celsius:";
+    cin>>celsius;
+
+
+    farenheit=(celsius * 9.0/5.0)+32;
+
+    cout<<"temperature in farenheit : "<<farenheit;
+    }
