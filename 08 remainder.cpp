ec2d06d552ef68e6abb0bede4ec1fe6712e2c053
sanjayks5200 <kssanjay5200@gmail.com>
2025-09-03 21:25:14 +0530

    Create 08 remainder.cpp

---
+++ 08 remainder.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int num1,num2,remainder;
+
+  cout<<"Enter the Divident and Devisor : "<<endl;
+  cin>>num1>>num2;
+
+  remainder=(num1%num2);
+  cout<<"The Remainder is  : "<<remainder<<endl;
+  return 0;
+}
