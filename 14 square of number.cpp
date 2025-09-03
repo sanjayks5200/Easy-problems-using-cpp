076aa73dcfc47eeee9f813fde890bcc92702b764
sanjayks5200 <kssanjay5200@gmail.com>
2025-09-03 22:26:00 +0530

    Create 14 square of number.cpp

---
+++ 14 square of number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num, square;
+    cout << "Enter the Number to find the Square: ";
+    cin >> num1;
+    sqaure=(num*num);
+    cout << "The Square of Number is : "<<square<<endl;
+  
+    return 0;
+}
