37ce01551e834ccb96576c1c747f62fa73c00483
sanjayks5200 <kssanjay5200@gmail.com>
2025-09-06 23:02:09 +0530

    Create 17 circle.cpp

---
+++ 17 circle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+ float radius,circum,pi=3.14;
+ cout<<"Enter the Radius : "<<endl;
+ cin>> radius;
+ circum=(2*pi*radius);
+ cout<<"Circumference of the Circle  : "<<circum<<endl;
+    return 0;
+}
