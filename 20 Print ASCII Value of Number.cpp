bfa21ff0d49c2aff6a30a463655949b4d886f6fd
sanjayks5200 <kssanjay5200@gmail.com>
2025-09-06 23:07:24 +0530

    Create 20 Print ASCII Value of Number.cpp

---
+++ 20 Print ASCII Value of Number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char num;
+
+    cout<<"Enter the Number : ";
+    cin>>num;
+    cout<<"ASCII Value is : "<<int(num)<<e;
+    
+    return 0;
+}
