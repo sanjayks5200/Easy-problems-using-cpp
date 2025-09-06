daf8ca0005b6130f52ab6e1b68d8e4684160cde1
sanjayks5200 <kssanjay5200@gmail.com>
2025-09-06 23:01:14 +0530

    Create 16 area of circle.cpp

---
+++ 16 area of circle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  float radius,area,pi=3.14;
+  cout<<"Ente the Radius : "<<endl;
+  cin>> radius;
+  area=pi*(radius*radius);
+  cout<<"Area of the Circle  : "<<area<<endl;
+  return 0;
+}
